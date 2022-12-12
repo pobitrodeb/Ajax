986f76e463db25fd601ecee413c591b9b1be0192
Pobitrodeb <pobitrodn@gmail.com>
2022-12-12 22:01:47 +0600

    send request and change text

--- Day_01/js/main.js
+++ Day_01/js/main.js
@@ -2,6 +2,9 @@ function loadData()
 {
     const xhr = new XMLHttpRequest(); 
     xhr.onload = function(){
-        const container = document.getElementById('demo')
-    }
+        const container = document.getElementById('demo'); 
+        demo.innerHTML = xhr.responseText; 
+    }; 
+    xhr.open("GET", "./data/data.txt");
+    xhr.send();
 }
