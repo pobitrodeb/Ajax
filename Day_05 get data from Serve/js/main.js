c9a33a911c1d00403e4f23eb3ce013ea288f20c4
Pobitrodeb <pobitrodn@gmail.com>
2022-12-14 21:19:14 +0600

    get data session complete

---
+++ Day_05 get data from Serve/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('#btnClick').click(function(){
+        $('#loadData').load('content.html', function(){
+            alert("This is done.."); 
+        });
+    })
+});
