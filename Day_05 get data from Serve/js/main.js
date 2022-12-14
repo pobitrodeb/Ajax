036be66f4029bcfd80bcaa39be1277361a9e71e2
Pobitrodeb <pobitrodn@gmail.com>
2022-12-14 22:05:07 +0600

    mini project 2 searc project initilize

--- Day_05 get data from Serve/js/main.js
+++ Day_05 get data from Serve/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('#btnClick').click(function(){
-        $('#loadData').load('content.html', function(){
+        $('#loadData').load('../content.html', function(){
             alert("This is done.."); 
         });
     })
---
+++ mini_Projects/02_Data Search/config.php
@@ -0,0 +1,6 @@
+<?php 
+	$con = mysqli_connect("localhost", "", "root", "02_data_search");
+    if(!$con){
+        echo "Database connection faild" . mysqli_connect_error();
+    }
+?>
